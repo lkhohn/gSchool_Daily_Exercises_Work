ffd2588342308eba858f24687066849245bb4b0d
LKHohn <lkhohn@gmail.com>
2015-11-13 15:51:43 -0700

    changed colors in JS

--- 2015.11.13 Crush Code Friday/app.js
+++ 2015.11.13 Crush Code Friday/app.js
@@ -1,3 +1,17 @@
+document.getElementById('pallette1').style.backgroundColor='#ffffff';
+document.getElementById('pallette2').style.backgroundColor='#cccccc';
+document.getElementById('pallette3').style.backgroundColor='#f73636';
+document.getElementById('pallette4').style.backgroundColor='#0a841c';
+document.getElementById('pallette5').style.backgroundColor='#e590f7';
+document.getElementById('pallette6').style.backgroundColor='#3f0e2a';
+document.getElementById('pallette7').style.backgroundColor='#729593';
+document.getElementById('pallette8').style.backgroundColor='#d97777';
+document.getElementById('pallette9').style.backgroundColor='#242961';
+document.getElementById('pallette10').style.backgroundColor='#752373';
+document.getElementById('pallette11').style.backgroundColor='#89eac3';
+document.getElementById('pallette12').style.backgroundColor='#324a15';
+document.getElementById('pallette12').style.backgroundColor='#324a15';
+
 var brushColor = 'red';
 window.addEventListener('click', function(event){
     var itemClicked = event.target;
