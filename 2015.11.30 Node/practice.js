5fc06329b86d9c16890d1cdee5b05a06bab4e471
LKHohn <lkhohn@gmail.com>
2015-11-30 14:42:07 -0700

    making multiple API requests

--- 2015.11.30 Node/practice.js
+++ 2015.11.30 Node/practice.js
@@ -1,9 +1,29 @@
+// var timeout = false;
+// function timer(){
+//     setTimeout(function(){
+//           timeout=true;
+//     },100);
+//     wait();
+//     console.log('done');
+// }
+// function wait(){
+//     if(!timeout){
+//         console.log('working....waiting');
+//         setTimeout(wait,0);
+//     }
+//     else {
+//         console.log('done');
+//     }
+// }
+// timer();
+
+
 var allData = [];
 function getReq (url, callback){
     var req = new XMLHttpRequest();
     req.onreadystatechange = function(){
     if(this.readyState===4 && this.status < 400) {
-        allData.push(JSON.parse(this.responseText));
+        allData.push(this.responseText);
         callback();
         }
     }
