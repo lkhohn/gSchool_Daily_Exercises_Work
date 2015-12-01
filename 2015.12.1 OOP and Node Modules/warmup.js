53087119b4997780fc2b47d7f87fb4ef147c1b1a
LKHohn <lkhohn@gmail.com>
2015-12-01 09:08:56 -0700

    first commit

---
+++ 2015.12.1 OOP and Node Modules/warmup.js
@@ -0,0 +1,12 @@
+// Todays morning warmup is nice and simple!
+// For the next 25 minutes you should research the Pomorodo technique.
+// As you read up on it keep in mind and answer the following questions:
+//
+// What is it?
+// How does it work?
+// Is there any scientific backing to it?
+// How could a programmer benefit from it?
+// Do you think those benefits are applicable to you?
+// Take a 5 minute break.
+//
+// Let's discuss what we learned.
