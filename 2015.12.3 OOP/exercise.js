ee9d21cba7e7125847f960d5e6cae58fdbca162e
LKHohn <lkhohn@gmail.com>
2015-12-03 16:55:38 -0700

    updated with polymorphism

--- 2015.12.3 OOP/exercise.js
+++ 2015.12.3 OOP/exercise.js
@@ -1,27 +1,110 @@
-var Name = function() {
-  var _firstName = 'Lindsay';
-  var _lastName = 'Hohn';
+// var Name = function() {
+//   var _firstName = 'Lindsay';
+//   var _lastName = 'Hohn';
+//   return {
+//     getfirstName: function() {
+//       return _firstName;
+//     },
+//     getLastName: function() {
+//       return _lastName;
+//     },
+//     setFirstName: function(newFirstName){
+//       _firstName = newFirstName;
+//     },
+//     setLastName: function(newLastName){
+//       _lastName = newLastName;
+//     }
+//   }
+// }
+// var myName = new Name();
+// console.log(myName.getfirstName());
+
+var Name = function(inputFirstName, inputLastName) {
+  var _firstName = inputFirstName;
+  var _lastName = inputLastName;
   return {
-    getfirstName: function() {
+    getFirstName: function() {
       return _firstName;
     },
     getLastName: function() {
       return _lastName;
     },
-    setFirstName: function(newFirstName){
-      _firstName = newFirstName;
-    },
-    setLastName: function(newLastName){
-      _lastName = newLastName;
+    getFullName: function() {
+      console.log('ERR: not implemented');
     }
-  }
-}
-var myName = new Name();
-console.log(myName.getfirstName());
+  };
+};
 
-var UserInput = function(){
-  myName.setFirstName = 'Beth';
+var NewName = function(inputFirstName, inputLastName) {
+  //invoke parent constructor to get interface and assign it to var input
+  var input = Name(inputFirstName, inputLastName);
 
+  //override interface property getFullName with a another function
+  input.getFullName = function() {
+    return input.getFirstName() + ' ' + input.getLastName();
+  };
+
+  //return interface
+  return input;
 }
-var userInputResult = new UserInput;
-console.log(userInputResult.getfirstName());
+
+var myNewName = NewName('Lindsay', 'Hohn');
+console.log(myNewName.getFullName());
+
+
+// polymorphorism
+// var Shape = function(newSize, newColor) {
+//   var _size = newSize;
+//   var _color = newColor;
+//   return {
+//     getArea: function() {
+//       console.log('ERR: not implemented');
+//     },
+//     getSize: function() {
+//       return _size;
+//     },
+//     getColor: function() {
+//       return _color;
+//     }
+//   };
+// };
+//
+// var Circle = function(newSize, newColor) {
+//   // call super constructor
+//   var self = Shape(newSize, newColor);
+//   self.getArea = function() {
+//     return 3.14 * self.getSize() * self.getSize();
+//   }
+//   return self;
+// };
+//
+// var myCircle = Circle(5, 'white');
+// // calling getArea on a child of Shape Object but get output for circle
+// console.log(myCircle.getArea());
+//
+//
+// var Square = function(newSize, newColor) {
+//   // call super constructor
+//   var self = Shape(newSize, newColor);
+//   self.getArea = function() {
+//     return self.getSize() * self.getSize();
+//   }
+//   return self;
+// };
+//
+// var mySquare = Square(5, 'white');
+// // calling getArea on a child of Shape Object but get output for square
+// console.log(mySquare.getArea());
+
+
+
+// polymorphorism -- one function that has different actions based on the input
+// function multiply(x, z){
+//   if(typeof x === 'number' && typeof z === 'number'){
+//     return x*z;
+//   }
+//   else if(x instanceof Vector2 && z instanceof Vector2){
+//     return x.multply(z);
+//   }
+// }
+// console.log(multiply(4,4));
