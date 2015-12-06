a58148f116fe95c890c08d64b69861339a0891a7
LKHohn <lkhohn@gmail.com>
2015-12-05 17:47:39 -0700

    working on crush code exercise

--- 2015.12.4 Crush Code Friday/practice.js
+++ 2015.12.4 Crush Code Friday/practice.js
@@ -52,3 +52,185 @@ var Chair = function(height, width, wheels) {
 var newChair = Chair(10, 10, 1);
 console.log(newChair.getWheels());
 console.log(newChair.comfort());
+
+
+
+//Entity this is the parent class
+function Entity() {
+  return {
+    energy: '0',
+    direction: 'n',
+    originChar: 'E',
+    act: function() {
+      return {
+        type: 'move',
+        direction: this.direction
+      };
+    }
+  };
+}
+
+function Animal() {
+  var self = new Entity();
+  self.originChar = 'A';
+  self.direction = 's';
+  self.energy = 10;
+  self.act = function(view) {
+    var returnObject;
+    var food = view.find('');
+    var emptySpace = view.find(' ');
+    var otherAnimal = view.find('A');
+    var predator = view.find('C');
+    if (predator) {
+
+    };
+    else if (food) {
+      returnObject = {
+        type: 'eat',
+        direction: food
+      };
+    } else if (emptySpace && otherAnimal && self.energy >= 100) {
+      returnObject = {
+        type: 'reproduce',
+        direction: emptySpace
+      };
+    } else if (emptySpace) {
+      self.direction = emptySpace;
+      returnObject = {
+        type: 'move',
+        direction: this.direction
+      };
+    }
+    return returnObject;
+  };
+  return self;
+}
+
+function Plant() {
+  var self = new Entity();
+  self.originChar = 'P';
+  delete self.direction;
+  self.energy = 10;
+  self.act = function(view) {
+    // grow is the default action
+    var returnAction = {
+      type: 'grow'
+    };
+    if (self.energy >= 40) {
+      // try to find an empty space
+      var emptySpace = view.find(' ');
+      // if one exists
+      if (emptySpace) {
+        // reproduce at that empty space
+        returnAction = {
+          type: 'reproduce',
+          direction: emptySpace
+        };
+      }
+    }
+    // return our action
+    return returnAction;
+  };
+  // return our new entity plant
+  return self;
+}
+
+function Carnivore() {
+  var self = new Entity();
+  self.originChar = 'C';
+  self.direction = 's';
+  self.energy = 10;
+  self.act = function(view) {
+    var returnObject;
+    var food = view.find('A');
+    var emptySpace = view.find(' ');
+    var otherAnimal = view.find('C');
+    if (food) {
+      returnObject = {
+        type: 'eat',
+        direction: food
+      };
+    } else if (emptySpace && otherAnimal && self.energy >= 100) {
+      returnObject = {
+        type: 'reproduce',
+        direction: emptySpace
+      };
+    } else if (emptySpace) {
+      self.direction = emptySpace;
+      returnObject = {
+        type: 'move',
+        direction: this.direction
+      };
+    }
+    return returnObject;
+  };
+  return self;
+}
+function Tree() {
+  var self = new Entity();
+  self.originChar = 'T';
+  delete self.direction;
+  self.energy = 10;
+  self.act = function(view) {
+    // grow is the default action
+    var returnAction = {
+      type: 'grow'
+    };
+    if (self.energy >= 40) {
+      // try to find an empty space
+      var emptySpace = view.find(' ');
+      // if one exists
+      if (emptySpace) {
+        // reproduce at that empty space
+        returnAction = {
+          type: 'reproduce',
+          direction: emptySpace
+        };
+      }
+    }
+    // return our action
+    return returnAction;
+  };
+  // return our new entity plant
+  return self;
+}
+
+
+//The wall element inherits from Entity
+function Wall() {
+  var self = new Entity();
+  //override act
+  self.act = false;
+  return self;
+}
+// Your code here
+
+
+//uncomment entities from the legend as you implement them
+var legend = {
+  "#": Wall,
+  "E": Entity,
+  "A": Animal,
+  "P": Plant,
+  "C": Carnivore,
+  "T": Tree
+};
+
+//to add an entity to the map replace an empty character with your entitycharacter
+var map = ["############################",
+  "#####      AP       P ######",
+  "##   C  P    P   P        ##",
+  "#    ##     A  P          ##",
+  "#     P  P    P   ##  P    #",
+  "#  P   A   P  P   ##   P   #",
+  "#     P  P     P  ##  C    #",
+  "#  C        #   P        P #",
+  "#  P  A   P #   C  P   A   #",
+  "#           ##      P      #",
+  "##    P    ###   P       ###",
+  "############################"
+];
+var world = new LifelikeWorld(map, legend);
+
+//Megaman EXECUTE! \o/
+animateWorld(world);
