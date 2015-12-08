9a6949c280242e25e0edba93781852a919ef9ab7
LKHohn <lkhohn@gmail.com>
2015-12-08 12:00:03 -0700

    first commit

---
+++ 2015.12.8 Node Modules and HTTP Modules/app.js
@@ -0,0 +1,7 @@
+//This is where you should incorporate your modules and execute your app
+var message = require('./autoResponse.js');
+
+// console.log(message);
+message.dayOfWeek('Monday');
+
+// Requirements: Must utilize your autoResponse.js module, 1 core module, and 1 module from NPM.
