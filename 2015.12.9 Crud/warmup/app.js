2ed7b3a94f39828fb575697af7de215aee25c54d
LKHohn <lkhohn@gmail.com>
2015-12-09 10:39:45 -0700

    finished warmup

--- 2015.12.9 Crud/warmup/app.js
+++ 2015.12.9 Crud/warmup/app.js
@@ -1,9 +1,34 @@
 'use strict';
 
-//TODO implement the jQuery ajax call here and console log the data
+// implement the jQuery ajax call here and console log the data
 
 //See https://api.jquery.com/jquery.get/ for an example of the get function
 
 //Alternatively
 
 //See http://api.jquery.com/jquery.ajax/ for an exmaple of the ajax function
+
+
+//
+// $.ajax({
+//   url: 'http://localhost:8000/api',
+//   method: 'GET',
+//   json: 'callback',
+//   dataType: 'JSON',
+//   success: function(data){
+//     // var stringData = JSON.stringify(data);
+//     // var parseData = JSON.parse(stringData);
+//     console.log(data);
+//   },
+//   error: function(errorObject, textStatus) {
+//     console.log(errorObject);
+//     console.log(textStatus);
+//   }
+// });
+$.ajax({
+  url: 'http://localhost:8000/api',
+  json: 'callback',
+  dataType: 'JSON'
+}).done(function main(data){
+    console.log(data);
+  });
