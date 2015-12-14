a195b84d3433a1fd8dce45c04a7c5f11173a7172
LKHohn <lkhohn@gmail.com>
2015-12-14 09:11:13 -0700

    first commit

---
+++ 2015.12.14 Express/warmup.js
@@ -0,0 +1,8 @@
+// Kewl Interpreter
+//
+// For some more practice with the filesystem module, we'll spend some time creating a simple interpreter.
+// An interpreter is a set of instructions that reads and handles code. Our interpreter will be simple and only handle one command - print.
+// Follow the instructions in this document to create an interpreter for the kewl.js file.
+//
+// After completing the basic interpreter, follow the instructions so that print takes parentheses and then so that some characters are escaped.
+// (Escaping a character just means that it's special meaning is ignored and the character is just output.)
