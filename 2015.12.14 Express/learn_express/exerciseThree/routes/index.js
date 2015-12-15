48664f02eee9a4d0a9165cd335451e19c4e20c88
LKHohn <lkhohn@gmail.com>
2015-12-15 10:50:41 -0700

    working on exercise three

---
+++ 2015.12.14 Express/learn_express/exerciseThree/routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var snippets = require('./lib/snippets.js');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', {
+    title: 'Hipstify',
+    subTitle: 'how to be hipsta-licious',
+    body: "write a sentence or two, and we'll hipstafy it for you!"
+  });
+});
+
+
+
+router.post('/', function(req, res, next) {
+  // console.log(req.body.textarea);
+  var input = req.body.textarea;
+  var words = input.split(" ");
+  console.log(words);
+  for (var i = 0; i < words.length; i++) {
+  }
+});
+
+
+
+module.exports = router;
---
+++ 2015.12.14 Express/learn_express/exerciseThree/routes/lib/snippets.js
@@ -0,0 +1,18 @@
+module.exports = [
+  "Portland pug",
+  "90s sustainable quinoa ",
+  "Artisan Thundercats drinking Pabst",
+  "chia readymade",
+  "flexitarian",
+  "lo-fi fashion",
+  "mustache Intelligentsia",
+  "Aesthetic keytar",
+  "hella beard boy",
+  "Beards",
+  "mixologist",
+  "craftsmania",
+  "keytar beard",
+  "Thundercats mullet",
+  "dayglo milk fat",
+  "zebras zebras lions and bears"
+];
