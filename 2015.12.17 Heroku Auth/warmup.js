dc2d042c376d0bcf048c1cf7aaff83bd6e3be924
LKHohn <lkhohn@gmail.com>
2015-12-17 10:04:21 -0700

    first commit

---
+++ 2015.12.17 Heroku Auth/warmup.js
@@ -0,0 +1,17 @@
+Sales Persons
+salesperson_id
+
+Sales
+salesperson_id
+id
+item_name
+item_price
+
+Sales Transaction
+salesperson_id
+id
+order_date
+delivery_date
+shipping_date
+payment_type
+payment_date
