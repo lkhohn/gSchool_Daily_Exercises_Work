301672e1d95825c24c34159fbf19fe7f209ee655
LKHohn <lkhohn@gmail.com>
2016-01-15 12:47:46 -0700

    basic CRUD functionality

--- 2016.1.15 Crush Code/crushCode/database/seed.sql
+++ 2016.1.15 Crush Code/crushCode/database/seed.sql
@@ -1,6 +1,7 @@
 INSERT INTO posts (id, category, title, content, user_id) VALUES (default, 'outdoors', 'walk in the woods', 'I spend alot of time walking in the woods', '1');
 INSERT INTO posts (id, category, title, content, user_id) VALUES (default, 'beer', 'beer in the woods', 'I spend alot of time drinking beer in the woods', '2');
 INSERT INTO posts (id, category, title, content, user_id) VALUES (default, 'dogs', 'dogs like to hike', 'I spend alot of time hiking with my dog in the woods', '3');
+INSERT INTO posts (id, category, title, content, user_id) VALUES (default, 'test', 'test', 'test', '4');
 
 
 
--- 2016.1.15 Crush Code/crushCode/routes/index.js
+++ 2016.1.15 Crush Code/crushCode/routes/index.js
@@ -1,9 +1,94 @@
 var express = require('express');
 var router = express.Router();
+var bcrypt = require('bcrypt');
+var pg = require('pg');
+var knex = require('knex')({
+  client:'pg',
+  connection: process.env.DATABASE_URL
+});
+
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  knex('posts').select().then(function(postDetails){
+    res.render('index', {
+      title: 'BlueIt',
+      postDetails: postDetails
+        });
+    });
 });
+
+/* POST create a new post */
+router.get('/addPost', function(req, res, next){
+res.render('addPost', {
+  });
+});
+
+router.post('/addPost', function(req, res, next){
+  knex('posts').insert({
+    id: req.body.id,
+    title: req.body.title,
+    content: req.body.content,
+    category: req.body.category,
+    user_id: req.body.user_id
+  }).then(function(postDetails){
+    res.redirect('/');
+  });
+});
+
+
+/* GET individual post page */
+router.get('/:id', function(req, res, next){
+  knex('posts').where('id', req.params.id).then(function(data){
+    res.render('individualPost', {
+      id: data[0].id,
+      title: data[0].title,
+      content: data[0].content,
+      category: data[0].category,
+      user_id: data[0].user_id,
+      editLink: req.params.id +'/editPost',
+      deleteLink: req.params.id
+    });
+  });
+});
+
+
+/* POST edit individual posts */
+router.get('/:id/editPost', function(req, res, next){
+  knex('posts').where('id', req.params.id).then(function(data){
+    res.render('editPost', {
+      id: data[0].id,
+      title: data[0].title,
+      content: data[0].content,
+      category: data[0].category,
+      user_id: data[0].user_id
+    });
+  });
+});
+
+router.post('/:id/editPost', function(req, res, next){
+  knex('posts').where('id', req.params.id).update({
+    id: req.body.id,
+    title: req.body.title,
+    content: req.body.content,
+    category: req.body.category,
+    user_id: req.body.user_id
+  }).then(function(postDetails){
+    res.redirect('/');
+  });
+});
+
+/* DELETE individual posts */
+router.post('/:id', function(req, res, next){
+  knex('posts').where('id', req.params.id).delete().then(function(){
+    res.redirect('/');
+  });
+});
+
+
+
+
 
 module.exports = router;
