cd825f2b7ee54f629b72da1118a5264a00f5a1ac
LKHohn <lkhohn@gmail.com>
2016-01-21 09:22:42 -0700

    angular homework

---
+++ 2016.1.20 Angular Events/angular-events-exercises/ngClick.js
@@ -0,0 +1,31 @@
+var app = angular.module('firstApp', []);
+
+app.controller("MyFirstController", function($scope){
+  $scope.pickRandomNumber = function(){
+    $scope.number = Math.floor(Math.random()*10+1);
+  };
+})
+
+app.controller('ReverseController', function($scope){
+  $scope.word = "";
+  $scope.wordReverser = function(){
+    // return $scope.word.split('').reverse.join('');
+  $scope.wordReversed = $scope.word.split('').reverse().join('');
+  };
+})
+
+app.controller("PingPong", function($scope){
+  $scope.addPlayerOnePoint = function(){
+    $scope.playerOnePoints = $scope.playerOnePoints + 1
+    if($scope.playerOnePoints != 11){
+      console.log('hey')
+    }
+  }
+  $scope.addPlayerTwoPoint = function(){
+    $scope.playerTwoPoints = $scope.playerTwoPoints + 1
+  }
+  $scope.reset = function(){
+    $scope.playerOnePoints = 0;
+    $scope.playerTwoPoints = 0;
+  }
+})
