9a824085b54e0f92d4189240c424514758f79626
LKHohn <lkhohn@gmail.com>
2016-01-21 16:53:36 -0700

    form validation class exercises

---
+++ 2016.1.21 Angular Validations/form validations/formController.js
@@ -0,0 +1,23 @@
+angular.module("app")
+		.controller("FormController", [FormController]);
+
+function FormController(){
+  var vm = this;
+  vm.users = [];
+  vm.displayUsers = displayUsers;
+
+  function displayUsers(){
+    var userDetails = {
+    username:vm.username,
+    password: vm.password,
+    email: vm.email,
+    zipCode: vm.zipCode
+    }
+    vm.users.push(userDetails);
+    vm.username='';
+    vm.password='';
+    vm.email='';
+    vm.zipCode='';
+
+  }
+}
