0f323befb2eb23dcf64e2529e4b6e65d1c6e2020
LKHohn <lkhohn@gmail.com>
2016-01-28 11:06:58 -0700

    first commit

---
+++ 2016.1.28 Custom Directives/customDirectiveExercise/js/directives.js
@@ -0,0 +1,6 @@
+angular.module('demoApp')
+.directive('lkhTestDirective', [function(){
+  return {
+    template: '<img src="http://i.imgur.com/ejBlwkVs.jpg">'
+};
+}]);
---
+++ 2016.1.28 Custom Directives/customDirectiveExercise/js/mainController.js
@@ -0,0 +1,6 @@
+angular.module('demoApp')
+.controller('MainController', [MainController]);
+
+function MainController(){
+  var vm = this;
+}
