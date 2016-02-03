4e0c124a5168699310e202f05a9f0ae31be80e1d
LKHohn <lkhohn@gmail.com>
2016-02-03 09:44:29 -0700

    first commit

---
+++ 2016.2.3 Custom Directives part 2/warmup/app.js
@@ -0,0 +1,38 @@
+function parser(input){
+  var stack = [];
+  if(input.length % 2 !== 0 ){
+    return false;
+  }
+  for(var i=0; i<input.length; i++){
+    var character = input[i];
+      if(character === '[' || character === '{' || character === '('){
+        stack.push(character);
+      }
+      else {
+        var stackCharacter = stack.pop();
+        switch (stackCharacter) {
+          case '[':
+            if(character !== ']'){
+              return false;
+            }
+            break;
+          case '{':
+            if(character !== '}'){
+              return false;
+            }
+            break;
+          case '(':
+            if(character !== ')'){
+              return false;
+            }
+            break;
+          default:
+            console.log('wtf');
+            return false;
+          }
+        }
+      }
+  return true;
+}
+console.log(parser('({[]})'));
+console.log(parser('({[}])'));
