10e59a6f9d59b7bdab14e9108e5c496c28ee4a0b
LKHohn <lkhohn@gmail.com>
2016-02-13 20:14:56 -0700

    crush code Thursday

--- 2016.2.11 Crush Code Thursday/gBooks/routes/authors.js
+++ 2016.2.11 Crush Code Thursday/gBooks/routes/authors.js
@@ -13,31 +13,22 @@ var knex = require('knex')({
 router.get('/', function(req, res, next){
   knex('authors').select()
   .leftOuterJoin('author_book', 'authors.author_id', 'author_book.author_id')
-  // .leftOuterJoin('books', 'books.book_id', 'author_book.book_id')
+  .leftOuterJoin('books', 'books.book_id', 'author_book.book_id')
   .then(function(postDetails){
-    console.log(postDetails);
+    // console.log(postDetails);
     res.render('authors', {
       postDetails:postDetails
     });
   });
 });
 
-// SELECT DISTINCT ON (author_book.author_id) from authors LEFT OUTER JOIN author_book ON authors.author_id = author_book.author_id;
+// SELECT * from authors LEFT OUTER JOIN author_book ON authors.author_id = author_book.author_id GROUP BY authors.author_id AS author_book.author_id;
+
 
 router.get('/newAuthor', function(req, res, next){
   res.render('newAuthor');
 });
 
-// router.post('/newAuthor', function(req, res, next){
-//   knex('authors').insert({
-//     firstname: req.body.firstname,
-//     lastname: req.body.lastname,
-//     portraiturl: req.body.portraiturl,
-//     biography: req.body.biography
-//   }).then(function(){
-//     res.redirect('/authors');
-//   });
-// });
 
 router.post('/newAuthor', function(req, res, next){
   knex('authors').returning('author_id').insert({
@@ -66,8 +57,8 @@ router.post('/newAuthor', function(req, res, next){
 
 router.get('/:author_id', function(req, res, next){
   knex('authors').select()
-  .innerJoin('author_book', 'authors.author_id', 'author_book.author_id')
-  .innerJoin('books', 'books.book_id', 'author_book.book_id')
+  .leftOuterJoin('author_book', 'authors.author_id', 'author_book.author_id')
+  .leftOuterJoin('books', 'books.book_id', 'author_book.book_id')
   .where('authors.author_id', req.params.author_id).first().then(function(result){
     // console.log(result);
         res.render('individualAuthor', { author:result });
@@ -75,18 +66,16 @@ router.get('/:author_id', function(req, res, next){
 });
 
 
-
 router.get('/:author_id/editAuthor', function(req, res, next){
   knex('authors').select()
-  .innerJoin('author_book', 'authors.author_id', 'author_book.author_id')
-  .innerJoin('books', 'books.book_id','author_book.book_id')
+  .leftOuterJoin('author_book', 'authors.author_id', 'author_book.author_id')
+  .leftOuterJoin('books', 'books.book_id','author_book.book_id')
   .where('authors.author_id', req.params.author_id).first()
   .then(function (result) {
       // console.log(result);
       res.render('editAuthor', {author: result});
     });
 });
-
 
 
 router.post('/:author_id/editAuthor', function(req, res, next){
@@ -107,16 +96,29 @@ router.post('/:author_id/editAuthor', function(req, res, next){
 
 
 
+// this delete works but not for both tables
+// router.post('/:author_id/delete', function(req, res, next){
+//   knex('author_book').where('author_id', req.params.author_id).del()
+//   .then(function(result){
+//     res.redirect('/authors');
+//   });
+// });
 
 router.post('/:author_id/delete', function(req, res, next){
-  knex('author_book').where('author_id', req.params.author_id).del()
+  knex('authors').select()
+  .where('authors.author_id', req.params.author_id).del()
   .then(function(result){
-    res.redirect('/authors');
+    knex('books').select()
+    .leftOuterJoin('author_book', 'authors.author_id', 'author_book.author_id')
+    .leftOuterJoin('books', 'books.book_id','author_book.book_id')
+    .then(function(){
+      res.redirect('/authors');
+    });
   });
 });
-
-
-
+// SELECT * from authors LEFT OUTER JOIN author_book ON authors.author_id = author_book.author_id;
+// DELETE FROM authors WHERE authors.author_id IN (SELECT author_book.author_id FROM author_book WHERE author_book.author_id = authors.author_id);
+// DELETE FROM books WHERE books.book_id IN (SELECT author_book.book_id FROM author_book WHERE author_book.book_id = books.book_id);
 
 
 
