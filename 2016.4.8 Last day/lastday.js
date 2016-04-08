79badd57690d129b546e85456985fc6fe01e3f26
LKHohn <lkhohn@gmail.com>
2016-04-08 12:46:57 -0600

    last day

---
+++ 2016.4.8 Last day/lastday.js
@@ -0,0 +1,13 @@
+// Advice from last day of class
+//
+// -Learn a statically/strictly typed language like Java, C Sharp or C++
+// -Always be the least smartest person on your team. If you are the smartest, you become too lazy and cocky
+//
+//
+// -Develop a strong plan for your employment
+// -what are you working on right now? how many hours will you work on this?
+// -how many jobs are you going to apply for?
+// -reading tech blogs or twitter
+// -practicing tech interview questions
+// -even if you are getting a 3rd or 4th interview, keep applying
+// -if rejected, can always ask 'what are you looking for? how can I improve as a candidate?"
